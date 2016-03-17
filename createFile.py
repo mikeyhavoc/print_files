09d7a1d1f3d974f790693a871929e7cb97185341
mikey <mikeyjhavoc@gmail.com>
2016-03-17 09:41:55 -0400

    comments add, imports, beginning of def.

--- createFile.py
+++ createFile.py
@@ -1,4 +1,13 @@
 # python3
 # author: Michael Williams
 # date: 3/17/16
-# description: create a text file and list all 
+# description: create file write to text file.
+# update:
+# version: 0.5
+import os,sys
+
+def create_text_file( items ):
+    '''creates text file for list of items
+       from folder you are outtputting from.
+    '''
+    text_file_name = input('text file name ')
