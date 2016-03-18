3026c04ccf7321355faabb7d944050f86bbcee41
mikey <mikeyjhavoc@gmail.com>
2016-03-18 06:08:06 -0400

    text create text file, add ext .txt., continue if directory

--- createFile.py
+++ createFile.py
@@ -6,10 +6,16 @@
 # version: 0.5
 import os,sys
 
-def create_text_file( text_file ):
+def create_text_file( folder ):
     '''creates text file for list of items
        from folder you are outtputting from.
     '''
+    path = folder
+    files = os.list( path )
+
     text_file_name = input('text file name ')
-    text_file = open(text_file_name, 'w') # creates text_file_name and writes to text_file
-    
+    print('creating text file..')
+    text_file = open(text_file_name + '.txt', 'w') # creates text_file_name and writes to text_file
+    for filenames in files:
+        if filenames.startswith('.'):
+            continue                   # skip directories
